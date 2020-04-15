96ecb63f0e3cbb5b02c5bd8658ca4548737e07bc
RiccardoCuccu <r.cuccu@outlook.it>
2020-04-16 01:24:15 +0200

    Bug fixing the return

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ def bop(bot, update):
     bot.send_photo(chat_id=chat_id, photo=url)
 
 def main():
-    updater = Updater(os.environ.get['TOKEN'])
+    updater = Updater(os.environ['TOKEN'])
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('bop',bop))
     updater.start_polling()
