797fcfff21cfd2b29d9f3d01ba3022e7c17f6a6c
Riccardo <r.cuccu@outlook.it>
2020-04-20 17:16:56 +0200

    Add some test function

--- bot.py
+++ bot.py
@@ -1,44 +1,58 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
-import requests
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
-import re
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)		# you will know when (and why) things don't work as expected
 
-def get_url():
-    contents = requests.get('https://random.dog/woof.json').json()    
-    url = contents['url']
-    return url
+updater = Updater(os.environ['TOKEN'], use_context=True)
+dispatcher = updater.dispatcher
 
-#def start(update, context):
-#    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-    
-#Send a message when the command /start is issued
-def start(bot, update):
-    update.message.reply_text('Hi!')
+def start(update, context):																					# start with a message
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Benvenuto nel bot di @UnicaNews!\nCome posso esserti utile?")
 
-def bop(bot, update):
-    url = get_url()
-    chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chat_id, photo=url)
+def echo(update, context):																					# echo all non-command messages it receives
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-def bup(bot, update):
-    url = get_url()
-    chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chat_id, photo=url)
+def caps(update, context):																					# take some text as an argument and reply to it in CAPS
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
-def main():
-    updater = Updater(os.environ['TOKEN'])
-    dispatcher = updater.dispatcher
-    
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
-    
-    dispatcher.add_handler(CommandHandler('bop',bop))
-    dispatcher.add_handler(CommandHandler('bup',bup))
-    updater.start_polling()
-    updater.idle()
+def hello(update, context):																					# say "Ciao USER_FIRST_NAME!"
+    update.message.reply_text('Ciao {}!'.format(update.message.from_user.first_name))
 
-if __name__ == '__main__':
-    main()
+def unknown(update, context):																				# reply to all commands that were not recognized by the previous handlers
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Mi dispiace, questo non e' un comando supportato.")
+
+start_handler = CommandHandler('start', start)
+
+dispatcher.add_handler(CommandHandler('start', start))														# responds to the /start command
+dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))   							# replies to any message without command
+dispatcher.add_handler(CommandHandler('caps', caps))														# responds to the /caps command
+dispatcher.add_handler(CommandHandler('hello', hello))														# responds to the /hello command
+dispatcher.add_handler(MessageHandler(Filters.command, unknown))											# responds to any unknown command
+
+
+#### INLINE COMMAND ###
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+def inline_caps(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+from telegram.ext import InlineQueryHandler
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
+########################
+
+
+updater.start_polling()
+updater.idle()
--- lib.sh
+++ lib.sh
@@ -2,3 +2,4 @@ sudo apt update
 sudo apt install python3-pip
 pip install python-telegram-bot --upgrade
 pip install requests
+pip install logging
